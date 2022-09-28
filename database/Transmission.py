ec0f61e196588b23707118cc38d8de34aaf1e766
paulpaulwoo <woo57@purdue.edu>
2022-09-27 22:15:48 -0400

    modified user added class that will handle transmission between database and backend

---
+++ database/Transmission.py
@@ -0,0 +1,20 @@
+import User
+import sqlite3
+
+class Database:
+
+    def __init__():
+        connect = sqlite3.connect("mydb.db")
+        cur = connect.cursor()
+
+    def DEV_FUNCTION_CREATE_NEW_TABLE():
+
+
+    
+
+
+    def save():
+        connect.commit()
+
+    def close():
+        connect.close()
--- database/User.py
+++ database/User.py
@@ -1,5 +1,3 @@
-
-
 class User:
     def __init__(self, id, username, password, email, dateofbirth, genderID):
         self.id = id
