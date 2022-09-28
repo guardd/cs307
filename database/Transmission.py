bc3790329f150c105e1e1bf67a64007e07e822dd
paulpaulwoo <woo57@purdue.edu>
2022-09-28 13:31:39 -0400

    Now database is able to: Insert user to database Search user in database by various methods, return it HAHAHAHAHAHHAHAAAHAHAHHAH I GOT IT TO WORK

--- database/Transmission.py
+++ database/Transmission.py
@@ -10,9 +10,13 @@ class Transmission:
     def search_by_id(self, id):
         self.cur.execute("SELECT * FROM 'User' WHERE id=?", (id,))
         user = self.cur.fetchone()
-        
+
         userobject = User(user[0], user[1], user[2], user[3], user[4], user[5])
         return userobject
+
+    def insert_user(self, user):
+        self.cur.execute("INSERT INTO 'User' VALUES(?,?,?,?,?,?)", (user.id, user.username, user.password, user.email, user.dateofbirth, user.genderID))
+        self.connect.commit()
 
     def search_by_username(self, username):
         self.cur.execute("SELECT * FROM 'User' WHERE username=?", (username,))
@@ -31,8 +35,8 @@ class Transmission:
     def add_user_to_database(self, user):
         self.cur.execute("INSERT INTO User VALUES(1000, 'example_username', 'example_password', 'example_email@email.com', '20010428', 'M')", )
 
-    def save():
-        connect.commit()
+    def save(self):
+        self.connect.commit()
 
     def close():
-        connect.close()
+        self.connect.close()
