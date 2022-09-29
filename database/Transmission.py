c56e0afe43af18afbc4d4c6ebad59d382122eeaf
paulpaulwoo <woo57@purdue.edu>
2022-09-28 20:08:10 -0400

    Added some error handling and login sequence

--- database/Transmission.py
+++ database/Transmission.py
@@ -3,40 +3,50 @@ import sqlite3
 
 class Transmission:
 
-    def __init__(self):
+    def __init__(self): #connects to db and makes a cursor
         self.connect = sqlite3.connect("mydb.db")
         self.cur = self.connect.cursor()
 
-    def search_by_id(self, id):
+    def search_by_id(self, id): #searches a user by id and returns the user
         self.cur.execute("SELECT * FROM 'User' WHERE id=?", (id,))
         user = self.cur.fetchone()
+        try:
+            userobject = User(user[0], user[1], user[2], user[3], user[4], user[5])
+            return userobject
+        except TypeError:
+            return -1 #couldn't find
 
-        userobject = User(user[0], user[1], user[2], user[3], user[4], user[5])
-        return userobject
-
-    def insert_user(self, user):
+    def insert_user(self, user): #inserts a user into the database and saves database
         self.cur.execute("INSERT INTO 'User' VALUES(?,?,?,?,?,?)", (user.id, user.username, user.password, user.email, user.dateofbirth, user.genderID))
         self.connect.commit()
 
-    def search_by_username(self, username):
+    def search_by_username(self, username):#searches a user by username and returns the user
         self.cur.execute("SELECT * FROM 'User' WHERE username=?", (username,))
         user = self.cur.fetchone()
-        a, b, c, d, e, f = user
-        userobject = User(a, b, c, d, e, f)
-        return userobject
+        try:
+            userobject = User(user[0], user[1], user[2], user[3], user[4], user[5])
+            return userobject
+        except TypeError:
+            return -1 #couldn't find
 
-    def search_by_password(self, password):
-        self.cur.execute("SELECT * FROM 'User' WHERE password=?", (password,))
+    def search_by_password(self, email):#searches a user by email and returns the user
+        self.cur.execute("SELECT * FROM 'User' WHERE password=?", (email,))
         user = self.cur.fetchone()
-        a, b, c, d, e, f = user
-        userobject = User(a, b, c, d, e, f)
+        userobject = User(user[0], user[1], user[2], user[3], user[4], user[5])
         return userobject
 
-    def add_user_to_database(self, user):
-        self.cur.execute("INSERT INTO User VALUES(1000, 'example_username', 'example_password', 'example_email@email.com', '20010428', 'M')", )
-
-    def save(self):
+    def save(self): #saves the database
         self.connect.commit()
 
-    def close():
+    def close(): #closes the connection to database
         self.connect.close()
+
+    def login_sequence(self, username, password): #returns user when correct, error code when not
+        user = self.search_by_username(username)
+        if user == -1:
+            return -1 #user nonexistant
+        else:
+            if user.password == password:
+                return user
+            else:
+                return -2 # password different
