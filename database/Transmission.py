5573513bf2ddbe5aacae7d861e986f1fc865d974
mburden97 <113633028+mburden97@users.noreply.github.com>
2022-10-06 23:39:33 -0400

    Added search for all data structures

--- database/Transmission.py
+++ database/Transmission.py
@@ -33,11 +33,46 @@ class Transmission:
         self.cur.execute("INSERT INTO 'Commodity' VALUES(?,?,?,?,?,?,?)", (commodity.name, commodity.type, commodity.id, commodity.portfolioID, commodity.userID, commodity.amount, commodity.avgUnitPrice))
         self.connect.commit()
     
+    def search_portfolio_by_id(self, id): #searches a portfolio by its unique id and returns it
+        self.cur.execute("SELECT * FROM 'Portfolios' WHERE id=?", (id,))
+        portfolio = self.cur.fetchone()
+        try:
+            portfolioobject = Portfolio(portfolio[0], portfolio[1], portfolio[2], portfolio[3], json.loads(portfolio[4]), json.loads(portfolio[5]), json.loads(portfolio[6]))
+            return portfolioobject
+        except TypeError:
+            return -1 #couldn't find
+    
+    def search_stock_by_id(self, id): #searches a stock by its unique id and returns it
+        self.cur.execute("SELECT * FROM 'Stock' WHERE id=?", (id,))
+        stock = self.cur.fetchone()
+        try:
+            stockobject = Stock(stock[0],stock[1],stock[2],stock[3],stock[4],stock[5],stock[6],stock[7])
+            return stockobject
+        except TypeError:
+            return -1 #couldn't find
+    
+    def search_property_by_id(self, id): #searches a property by its unique id and returns it
+        self.cur.execute("SELECT * FROM 'Property' WHERE id=?", (id,))
+        property = self.cur.fetchone()
+        try:
+            propertyobject = Property(property[0],property[1],property[2],property[3],property[4],property[5],property[6])
+            return propertyobject
+        except TypeError:
+            return -1 #couldn't find
+    def search_commodity_by_id(self, id): #searches a commodity by its unique id and returns it
+        self.cur.execute("SELECT * FROM 'Commodity' WHERE id=?", (id,))
+        commodity = self.cur.fetchone()
+        try:
+            commodityobject = Commodity(commodity[0],commodity[1],commodity[2],commodity[3],commodity[4],commodity[5],commodity[6])
+            return commodityobject
+        except TypeError:
+            return -1 #couldn't find
+
     def search_user_by_id(self, id): #searches a user by id and returns the user
         self.cur.execute("SELECT * FROM 'User' WHERE id=?", (id,))
         user = self.cur.fetchone()
         try:
-            userobject = User(user[0], user[1], user[2], user[3], user[4], user[5])
+            userobject = User(user[0], user[1], user[2], user[3], user[4], user[5], json.loads(user[6]))
             return userobject
         except TypeError:
             return -1 #couldn't find
@@ -54,7 +89,7 @@ class Transmission:
     def search_user_by_password(self, email):#searches a user by email and returns the user
         self.cur.execute("SELECT * FROM 'User' WHERE password=?", (email,))
         user = self.cur.fetchone()
-        userobject = User(user[0], user[1], user[2], user[3], user[4], user[5])
+        userobject = User(user[0], user[1], user[2], user[3], user[4], user[5], json.loads(user[6]))
         return userobject
     
     def insert_notification(self, notification):
@@ -66,7 +101,7 @@ class Transmission:
         notifications = self.cur.fetchall()
         notificationlist = []
         for notification in notifications:
-            notificationlist.append(Notification(notification[0], notification[1], notification[2], notification[3], notification[4]))
+            notificationlist.append(Notifications(notification[0], notification[1], notification[2], notification[3], notification[4]))
         return notificationlist
 
 
