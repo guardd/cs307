a19f3c1a87589a431a61ac9c9428578da3f16e14
guardd <89743227+guardd@users.noreply.github.com>
2022-10-09 17:46:14 -0500

    Update contact.js

--- project-website/src/pages/contact.js
+++ project-website/src/pages/contact.js
@@ -1,12 +1,41 @@
-import React from "react";
-  
+import React, {useEffect, useState} from "react";
 const Contact = () => {
-  return (
-    <div>
-      <h1>
-        Enter information for contact
-      </h1>
-    </div>
+  const [name, pushName]=useState(null)
+  const [contact, pushContact]=useState(null)
+
+  
+  return (  
+    <div className="contact-container">
+        <form className="contact-Form">
+          <div className="contact-content">
+            <h1 className="contact-title">Sign Up</h1>
+            <div className="contact-Name">
+              <label>Name</label>
+              <input type="text" onChange={pushName} 
+              className="contact-name-input" placeholder="Enter Your Name"/>
+            </div>
+            <div className="contact-contact">
+              <label>Email</label>
+              <input type="text" onChange={pushContact} 
+              className="contact-contact-input" placeholder="Enter Your Email"/>
+            </div>
+            <div className="contact-password">
+              <label>Problem</label>
+              <textarea 
+              className="contact-password-input" placeholder="Enter Password" rows="4" cols="50"/>
+            </div>
+            <div className="contact-button">
+              <button type="submit" className="contact-button-button"> 
+              Submit
+              </button>
+            </div>
+          </div>
+        </form>
+        
+        <h1>
+          
+        </h1>
+      </div>
   );
 };
   
