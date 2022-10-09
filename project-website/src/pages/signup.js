9c0945b4ab2baa812671a734ef3693c3a043424b
guardd <89743227+guardd@users.noreply.github.com>
2022-10-09 17:45:52 -0500

    Update signup.js

--- project-website/src/pages/signup.js
+++ project-website/src/pages/signup.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
   
 const SignUp = () => {
-  return (
-    <div>
-      <h1>
-        SignUp here
-      </h1>
-    </div>
+  const [username, pushUsername]=useState(null)
+  const [password, pushPassword]=useState(null)
+  function pushUsername(val)
+  {
+    pushUsername(val.target.value)
+  }
+  function pushPassword(val)
+  {
+    pushPassword(val.target.value)
+  }
+  
+  return (  
+    <div className="signUp-container">
+        <form className="signUp-Form">
+          <div className="signUp-content">
+            <h1 className="signUp-title">Sign Up</h1>
+            <div className="signUp-username">
+              <label>Username</label>
+              <input type="text" onChange={pushUsername} 
+              className="signUp-username-input" placeholder="Enter Username"/>
+            </div>
+            <div className="signUp-password">
+              <label>Password</label>
+              <input type="text" onChange={pushPassword} 
+              className="signUp-password-input" placeholder="Enter Password"/>
+            </div>
+            <div className="signUp-button">
+              <button type="submit" className="signUp-button-button"> 
+              SignUp
+              </button>
+            </div>
+          </div>
+        </form>
+        
+        <h1>
+          
+        </h1>
+      </div>
   );
 };
   
