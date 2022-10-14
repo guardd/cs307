ff6dd323ea6c755171a974e9b84106537c860768
guardd <89743227+guardd@users.noreply.github.com>
2022-10-14 13:26:18 -0400

    Update index.js

--- project-website/src/pages/index.js
+++ project-website/src/pages/index.js
@@ -1,13 +1,25 @@
 import React from "react";
-  
+import './index.css';
+import home from "./Images/home.jpg";
+
 const Home = () => {
   return (
     <div>
-      <h1>
-        Home
-      </h1>
+      <img
+        src={home}
+        alt="car"
+      />
+      <div className= 'about'>
+        <h1 className='about-title'>Who We Are</h1>
+        <p className='about-words'>This is who we are</p>
+      </div>
+      <div className= 'mission'>
+        <h1 className='mission-title'>Our Mission</h1>
+        <p className='about-words'>This is our Mission</p>
+      </div>
     </div>
   );
+
 };
   
 export default Home;
