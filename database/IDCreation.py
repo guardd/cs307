3c1d8ee287f1947fdb7c73e52e42eeef2c87400a
mburden97 <113633028+mburden97@users.noreply.github.com>
2022-11-02 23:05:22 -0400

    Add files via upload

--- database/IDCreation.py
+++ database/IDCreation.py
@@ -1,6 +1,12 @@
-import uuid 
+import uuid
+import os,binascii
 class IDCreation:
 
 	def generate_ID():
 		x = str(uuid.uuid4())
 		return x
+	def generate_color_hex():
+		hexcode = binascii.b2a_hex(os.urandom(6))
+		hexcode=hexcode.upper()
+		hexcode= f'#{hexcode}'
+		return hexcode
